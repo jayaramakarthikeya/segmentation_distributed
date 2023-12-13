64cc840c0994a014a1e4a55ac4f1af6c6bf5a3cf
JayaramKarthik <karthikeyaj82@gmail.com>
2023-12-12 22:08:11 -0500

    exp1

--- trainer/base_trainer.py
+++ trainer/base_trainer.py
@@ -147,7 +147,7 @@ class BaseTrainer:
                 #print("Parallel type None)))))))") 
 
             #try:
-            with torch.autocast(device_type = 'cuda', dtype=torch.float16,enabled=False):
+            with torch.autocast(device_type = 'cuda', dtype=torch.float16,enabled=True):
                 #print("heufheuf@@@@")
                 #FORWARD PASS
                 self.optimizer.zero_grad()
