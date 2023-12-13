0f5a2b7d14c1020140e3176ffaf8b9a25c0735a0
JayaramKarthik <karthikeyaj82@gmail.com>
2023-12-12 21:58:41 -0500

    exp1

--- trainer/base_trainer.py
+++ trainer/base_trainer.py
@@ -147,7 +147,7 @@ class BaseTrainer:
                 #print("Parallel type None)))))))") 
 
             try:
-                with torch.autocast(device_type = self.device, dtype=torch.float16,enabled=True):
+                with torch.autocast(device_type = self.device, dtype=torch.float16,enabled=False):
                     #print("heufheuf@@@@")
                     #FORWARD PASS
                     self.optimizer.zero_grad()
