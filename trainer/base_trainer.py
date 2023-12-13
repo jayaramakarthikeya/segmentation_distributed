3c5173c9a2a5d419fad290a9a24eeb1d2a481389
JayaramKarthik <karthikeyaj82@gmail.com>
2023-12-12 21:38:51 -0500

    exp1

--- trainer/base_trainer.py
+++ trainer/base_trainer.py
@@ -146,7 +146,7 @@ class BaseTrainer:
             #if self.parallel_type == None:
                 #print("Parallel type None)))))))") 
 
-            """ try:
+            try:
                 with torch.autocast(device_type='cuda', dtype=torch.float16,enabled=True):
 
                     #FORWARD PASS
@@ -173,11 +173,11 @@ class BaseTrainer:
                     print(loss.item())
 
             except RuntimeError:
-                continue """
+                continue
 
                     
             #else:
-            self.optimizer.zero_grad()
+            """ self.optimizer.zero_grad()
             output = self.model(images)
             #print("************in DP train loop")
             if self.model.module.model_type[:3] == "PSP":
@@ -195,7 +195,7 @@ class BaseTrainer:
             loss.backward()
             self.optimizer.step()
             #print("********",loss.item().dtype)
-            self.total_loss.update(loss.item())
+            self.total_loss.update(loss.item()) """
 
 
             
